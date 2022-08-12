8301e0f2ab1b850b9f101f146ef0fd3efcf17561
SupaDylan11 <91839242+SupaDylan11@users.noreply.github.com>
2022-08-12 15:34:34 +0100

    Completed the sign-in page file, and the NUNIT class for carrying out the NUNIT tests.

--- SD_TestAutomationFramework/SD_TestAutomationFramework/lib/pages/SD_SigninPage.cs
+++ SD_TestAutomationFramework/SD_TestAutomationFramework/lib/pages/SD_SigninPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace SD_TestAutomationFramework.lib.pages
 {
-    internal class SD_SigninPage
+    public class SD_SigninPage
     {
+        #region properties
+        //WEB DRIVER
+        private IWebDriver _seleniumDriver;
+        private string _signInUrl = AppConfigReader.BaseUrl;
+        private IWebElement _usernameLoginField => _seleniumDriver.FindElement(By.Id("user-name"));
+        private IWebElement _passwordLoginField => _seleniumDriver.FindElement(By.Id("password"));
+        private IWebElement _submitBtn => _seleniumDriver.FindElement(By.Id("login-button"));
+        private IWebElement _signinAlert => _seleniumDriver.FindElement(By.Id(""));
+        #endregion
+
+        //constructor
+        public SD_SigninPage(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+
+        #region methods
+        public void InputUserName(string username) => _usernameLoginField.SendKeys(username);
+        public void InputPassword(string password) => _passwordLoginField.SendKeys(password);
+        public void clickSignIn() => _submitBtn.Click();
+        public string GetAlertSignIn() => _signinAlert.Text;
+        #endregion
     }
 }
---
+++ SD_TestAutomationFramework/SD_TestAutomationFramework/tests/SigninPageTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using SD_TestAutomationFramework.lib;
+
+namespace SD_TestAutomationFramework.tests
+{
+    public class SigninPageTests
+    {
+        private SD_Website<ChromeDriver> SD_Website = new();
+
+        [Test]
+        public void GivenIAmOnTheHomePage_WhenIClickTheSIgninButton_ThenIShouldLandOnTheSignInPage()
+        {
+
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            SD_Website.SeleniumDriver.Quit();
+            SD_Website.SeleniumDriver.Dispose();
+        }
+    }
+}
