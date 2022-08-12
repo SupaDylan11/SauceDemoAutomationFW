25a8530ec6d887f37051e428698e19d6a7e4db79
Kenny <kharvey@spartaglobal.com>
2022-08-12 16:31:26 +0100

    Updated products POM and added unit tests

--- SD_TestAutomationFramework/SD_TestAutomationFramework/lib/pages/SD_ProductsPage.cs
+++ SD_TestAutomationFramework/SD_TestAutomationFramework/lib/pages/SD_ProductsPage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace SD_TestAutomationFramework.lib.pages
 {
-    internal class SD_ProductsPage
+    public class SD_ProductsPage
     {
+        private IWebDriver _seleniumDriver;
+        private IWebElement _backpackLink => _seleniumDriver.FindElement(By.ClassName("inventory_item_name"));
+        private IWebElement _filterLink => _seleniumDriver.FindElement(By.ClassName("product_sort_container"));
+        private IWebElement _filterZA => _filterLink.FindElement(By.XPath("//option[text() = 'Name (Z to A)']"));
+        private IWebElement _firstItem => _seleniumDriver.FindElement(By.ClassName("inventory_list")).FindElement(By.XPath("/*[1]"));
+
+        public SD_ProductsPage(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+
+        public void ClickBackpackLink() => _backpackLink.Click();
+        public void ClickFilterLink() => _filterLink.Click();
+        public void ClickFilterZA() => _filterZA.Click();
+        public string FirstItemName() => _firstItem.Text;
     }
 }
