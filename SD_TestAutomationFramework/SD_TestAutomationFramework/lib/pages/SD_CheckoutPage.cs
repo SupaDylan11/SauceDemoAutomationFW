9e01896666e62263bd4bcab155fcd607a3b8ba29
kenny95h <108397966+kenny95h@users.noreply.github.com>
2022-08-15 11:30:33 +0100

    Revert "added checkout tests and pages"

---
+++ SD_TestAutomationFramework/SD_TestAutomationFramework/lib/pages/SD_CheckoutPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+
+
+namespace SD_TestAutomationFramework.lib.pages
+{
+    public class SD_CheckoutPage
+    {
+        #region properties
+        //WEB DRIVER
+        private IWebDriver _seleniumDriver;
+        private IWebElement _firstNameLoginField => _seleniumDriver.FindElement(By.Id("first-name"));
+        private IWebElement _lastNameLoginField => _seleniumDriver.FindElement(By.Id("last-name"));
+        private IWebElement _postalCodeLoginField => _seleniumDriver.FindElement(By.Id("postal-code"));
+        private IWebElement _checkoutButton => _seleniumDriver.FindElement(By.Id("continue"));
+        private IWebElement _checkoutAlert => _seleniumDriver.FindElement(By.ClassName("error-button"));
+        #endregion
+
+        //constructor
+        public SD_CheckoutPage(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+
+        #region 
+        public void InputFirstName(string firstName) => _firstNameLoginField.SendKeys(firstName);
+        public void InputLastName(string lastName) => _lastNameLoginField.SendKeys(lastName);
+        public void InputPostCode(string postCode) => _postalCodeLoginField.SendKeys(postCode);
+        public void clickCheckout() => _checkoutButton.Click();
+        public string GetCheckoutAlert() => _checkoutAlert.Text;
+        #endregion
+    }
+}
